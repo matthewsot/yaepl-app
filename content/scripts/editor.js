68cc445f6c77646a6e423ff6483100e05633c3cf
Matthew Sotoudeh <matthewsot@outlook.com>
2017-07-26 11:06:41 -0700

    switch between languages for the codemirror

--- content/scripts/editor.js
+++ content/scripts/editor.js
@@ -1,20 +1,25 @@
+window.lang = window.location.hash.split('#')[1];
+
+var langDict = { "yaepl": "YAEPL", "javascript": "Javascript" };
+document.getElementById("lang").textContent = langDict[window.lang];
+
 var editorArea = document.getElementById("editor");
 var editor = CodeMirror.fromTextArea(editorArea, {
     lineNumbers: true,
     autofocus: true,
-    mode: "yaepl",
+    mode: window.lang,
 });
 
-editor.setValue(localStorage.getItem("cached-program") || "");
+/*editor.setValue(localStorage.getItem("cached-program") || "");
 editor.on("change", function () {
     localStorage.setItem("cached-program", editor.getValue());
-});
-    
+});*/
+
 document.getElementById("terminal").innerHTML = "";
 var term = new Terminal();
 document.getElementById("terminal").appendChild(term.html);
 
-function run() {
+function runYAEPL() {
     term.clear();
 
     var lines = editor.getValue().split('\n');
@@ -40,6 +45,42 @@ function run() {
     }
     interpretLine(0);
 };
+function runJS() {
+    term.clear();
+
+    var lines = editor.getValue().split('\n');
+    
+    var yaepl = new Yaepl({
+        outHandle: function (str) { window.term.print(str); },
+        promptHandle: function (str, callback) {
+            window.term.input(str, function (ret) {
+                callback(ret);
+            });
+        }
+    });
+
+    var l = 0;
+    var maxL = lines.length;
+    function interpretLine(l) {
+        yaepl.interpretLine(lines[l], function () {
+            l++;
+            if (l < maxL) {
+                interpretLine(l);
+            }
+        });
+    }
+    interpretLine(0);
+};
+function run() {
+    switch (window.lang) {
+        case "yaepl":
+            runYAEPL();
+            break;
+        case "javascript":
+            runJS();
+            break;
+    }
+}
 
 document.getElementById("run").onclick = run;
 editor.setOption("extraKeys", {
---
+++ content/scripts/frame.js
@@ -0,0 +1,6 @@
+var lis = document.querySelectorAll("nav > ul > li");
+for (var l = 0; l < lis.length; l++) {
+    lis[l].onclick = function () {
+        document.getElementById("view").src = "editor.html#" + this.getAttribute("lang");
+    };
+}
